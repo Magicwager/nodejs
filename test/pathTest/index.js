80d1b08b09105ba4673c23b71281cfd2046476ff
wujiank <wujiank@yonyou.com>
2017-12-30 23:37:35 +0800

    path示例

---
+++ test/pathTest/index.js
@@ -0,0 +1,10 @@
+const path=require('path')
+let cache = {};
+
+  function store(key, value) {
+      cache[path.normalize(key)] = value;
+  }
+
+  store('foo/bar', 1);
+  store('foo//baz//../bar', 2);
+  console.log(cache);  // => { "foo/bar": 2 }
